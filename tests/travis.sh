55cc0b6293d774d028bb047d541dc205f72aed44
Dylan K. Taylor <odigiman@gmail.com>
2017-02-04 11:34:55 +0000

    Rewritten Travis Test to be more foolproof

--- tests/travis.sh
+++ tests/travis.sh
@@ -17,30 +17,30 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-rm server.log 2> /dev/null
-mkdir -p ./plugins
+cd tests/plugins
+"$PHP_BINARY" ./PocketMine-DevTools/src/DevTools/ConsoleScript.php --make ./PocketMine-DevTools --relative ./PocketMine-DevTools --out ../../DevTools.phar
+cd ../..
 
-cp -r tests/plugins/PocketMine-DevTools ./plugins
-
-"$PHP_BINARY" ./plugins/PocketMine-DevTools/src/DevTools/ConsoleScript.php --make ./plugins/PocketMine-DevTools --relative ./plugins/PocketMine-DevTools --out ./plugins/DevTools.phar
-rm -rf ./plugins/PocketMine-DevTools
-
-echo -e "version\nmakeserver\nstop\n" | "$PHP_BINARY" src/pocketmine/PocketMine.php --no-wizard --disable-ansi --disable-readline --debug.level=2
-if ls plugins/DevTools/PocketMine*.phar >/dev/null 2>&1; then
+"$PHP_BINARY" DevTools.phar --make src,vendor --relative ./ --entry src/pocketmine/PocketMine.php --out PocketMine-MP.phar
+if [ -f PocketMine-MP.phar ]; then
     echo Server phar created successfully.
 else
-    echo No phar created!
+    echo Server phar was not created!
     exit 1
 fi
 
+
+mkdir plugins
+mv DevTools.phar plugins
 cp -r tests/plugins/PocketMine-TesterPlugin ./plugins
+
 "$PHP_BINARY" src/pocketmine/PocketMine.php --no-wizard --disable-ansi --disable-readline --debug.level=2
 
 result=$(grep 'TesterPlugin' server.log | grep 'Finished' | grep -v 'PASS')
 if [ "$result" != "" ]; then
-   echo "$result"
-   echo Some tests did not complete successfully, changing build status to failed
-   exit 1
+    echo "$result"
+    echo Some tests did not complete successfully, changing build status to failed
+    exit 1
 else
     echo All tests passed
 fi
