e763d4129b2abb8f0872edeb69dd7db1ae179c76
Dylan K. Taylor <odigiman@gmail.com>
2017-03-20 16:30:56 +0000

    Improved Travis Test, capture error output from console

--- tests/travis.sh
+++ tests/travis.sh
@@ -21,6 +21,8 @@ cd tests/plugins
 "$PHP_BINARY" ./PocketMine-DevTools/src/DevTools/ConsoleScript.php --make ./PocketMine-DevTools --relative ./PocketMine-DevTools --out ../../DevTools.phar
 cd ../..
 
+rm PocketMine-MP.phar
+
 "$PHP_BINARY" DevTools.phar --make src,vendor --relative ./ --entry src/pocketmine/PocketMine.php --out PocketMine-MP.phar
 if [ -f PocketMine-MP.phar ]; then
     echo Server phar created successfully.
@@ -29,8 +31,7 @@ else
     exit 1
 fi
 
-
-mkdir plugins
+mkdir plugins 2> /dev/null
 mv DevTools.phar plugins
 cp -r tests/plugins/PocketMine-TesterPlugin ./plugins
 
@@ -41,6 +42,9 @@ if [ "$result" != "" ]; then
     echo "$result"
     echo Some tests did not complete successfully, changing build status to failed
     exit 1
+elif [ $(grep -c "ERROR\|CRITICAL\|EMERGENCY" server.log) -ne 0 ]; then
+	echo Server log contains error messages, changing build status to failed
+	exit 1
 else
     echo All tests passed
 fi
