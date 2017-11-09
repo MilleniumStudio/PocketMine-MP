fc04e885a50d2284bec9e19429d351e2087a9bba
HoverEpic <vincent_c@fat.sh>
2017-11-09 18:31:55 +0100

    fix AddPaintingPacket

--- src/pocketmine/network/mcpe/protocol/AddPaintingPacket.php
+++ src/pocketmine/network/mcpe/protocol/AddPaintingPacket.php
@@ -53,7 +53,7 @@ class AddPaintingPacket extends DataPacket{
 	protected function encodePayload(){
 		$this->putEntityUniqueId($this->entityUniqueId ?? $this->entityRuntimeId);
 		$this->putEntityRuntimeId($this->entityRuntimeId);
-		$this->putVector3Obj($this->position);
+		$this->putBlockPosition($this->position->getFloorX(), $this->position->getFloorY(), $this->position->getFloorZ());
 		$this->putVarInt($this->direction);
 		$this->putString($this->title);
 	}
