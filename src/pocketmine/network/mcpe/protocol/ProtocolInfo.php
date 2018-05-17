68111ea3e8872741421edf4c702c9c81dd4e2315
Naphtaline <le.tux.tueur@gmail.com>
2018-05-17 18:43:38 +0200

    [CORE] Update protocole info

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -40,15 +40,15 @@ interface ProtocolInfo{
 	 * Actual Minecraft: PE protocol version
 	 */
 
-	public const CURRENT_PROTOCOL = 223;
+	public const CURRENT_PROTOCOL = 261;
 	/**
 	 * Current Minecraft PE version reported by the server. This is usually the earliest currently supported version.
 	 */
-	public const MINECRAFT_VERSION = 'v1.2.13';
+	public const MINECRAFT_VERSION = 'v1.4.0';
 	/**
 	 * Version number sent to clients in ping responses.
 	 */
-	public const MINECRAFT_VERSION_NETWORK = '1.2.13';
+	public const MINECRAFT_VERSION_NETWORK = '1.4.0';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
